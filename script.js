cb12dcf1ccd57abadf4aaa4d3d10c9e278b2e48d
Otsutsuki13 <Otsutsuki13>
2025-01-01 01:54:18 +0800

    Add existing project files to Git

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ import ScratchToRevealDemo from "./ScratchToRevealDemo";
 // Render the ScratchToRevealDemo component
 ReactDOM.render(<ScratchToRevealDemo />, document.getElementById("scratch-to-reveal-demo"));
 
+document.addEventListener('DOMContentLoaded', function () {
+  var scene = document.getElementById('projects');
+  var parallaxInstance = new Parallax(scene);
+});
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
